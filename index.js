c6dbb0fcea56e4b8e80b33fc1b7a42084c825cb9
Michael Wuergler <wuergler@gmail.com>
2016-03-10 17:19:16 +0000

    refactor

--- index.js
+++ index.js
@@ -1,46 +1,39 @@
 'use strict';
+var beeps = ['Bip', 'Beeep', 'Bop', 'Beeeep', 'Boop', 'Booop', 'Blipp', 'Beeeeep', 'Boooop', 'Bopp'];
 
-module.exports = (function () {
-	var beeps = ['Bip', 'Beeep', 'Bop', 'Beeeep', 'Boop', 'Booop', 'Blipp', 'Beeeeep', 'Boooop', 'Bopp'];
+module.exports.encode = function (text) {
+	var output = [];
 
-	function encode(text) {
-		var output = [];
-		for (var char in text) {
-			if (text.hasOwnProperty(char)) {
-				var encoded = [];
-				var charCodes = text[char].charCodeAt(0).toString().split('');
-				for (var code in charCodes) {
-					if (charCodes.hasOwnProperty(code)) {
-						encoded.push(beeps[charCodes[code]]);
-					}
+	for (var char in text) {
+		if (text.hasOwnProperty(char)) {
+			var encoded = [];
+			var charCodes = text[char].charCodeAt(0).toString().split('');
+			for (var code in charCodes) {
+				if (charCodes.hasOwnProperty(code)) {
+					encoded.push(beeps[charCodes[code]]);
 				}
-				output.push(encoded.join(' '));
 			}
+			output.push(encoded.join(' '));
 		}
-		return output.join('. ');
 	}
+	return output.join('. ');
+};
 
-	function decode(text) {
-		var output = [];
+module.exports.decode = function (text) {
+	var output = [];
 
-		var encoded = text.split('. ');
-		for (var char in encoded) {
-			if (encoded.hasOwnProperty(char)) {
-				var codes = encoded[char].split(' ');
-				var character = [];
-				for (var i in codes) {
-					if (codes.hasOwnProperty(i)) {
-						character.push(beeps.indexOf(codes[i]));
-					}
+	var encoded = text.split('. ');
+	for (var char in encoded) {
+		if (encoded.hasOwnProperty(char)) {
+			var codes = encoded[char].split(' ');
+			var character = [];
+			for (var i in codes) {
+				if (codes.hasOwnProperty(i)) {
+					character.push(beeps.indexOf(codes[i]));
 				}
-				output.push(String.fromCharCode(character.join('')));
 			}
+			output.push(String.fromCharCode(character.join('')));
 		}
-		return output.join('');
 	}
-
-	return {
-		encode: encode,
-		decode: decode
-	};
-})();
+	return output.join('');
+};
